effac3f1e60f7c3a9163cf6d6453434d47bf3f90
张祎荻 <479663032@qq.com>
2019-07-23 17:03:47 +0800

    流程

---
+++ Dtol/dtol/Flow_Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dtol.dtol
+{
+    public class Flow_Node
+    {
+        public string Id;
+        /// <summary>
+        /// 当前节点id
+        /// </summary>
+        public int Flow_NodeDefineId;
+        public Flow_NodeDefine Flow_NodeDefine;
+        /// <summary>
+        /// 父节点id
+        /// </summary>
+        public int Parent_Flow_NodeDefineId;
+        public Flow_NodeDefine Parent_Flow_NodeDefine;
+        /// <summary>
+        /// 当前流程id
+        /// </summary>
+        public int Flow_ProcedureId;
+        public Flow_Procedure  Flow_Procedure;
+        /// <summary>
+        /// 父流程id
+        /// </summary>
+        public int Parent_Flow_ProcedureId;
+        public Flow_Procedure Parent_Flow_Procedure;
+        public string status;
+        public int User_InfoId;
+        public User_Info User_Info;
+        public int Pre_User_InfoId;
+        public User_Info Pre_User_Info;
+        public DateTime? StartTime;
+        public DateTime? EndTime;
+    }
+}
---
+++ Dtol/dtol/Flow_Procedure.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dtol.dtol
+{
+    public class Flow_Procedure
+    {
+    }
+}
