5749112e5795dc8dca912ebf2f541d7f7dfa76c2
健 柴 <1054254369@qq.com>
2019-07-23 16:26:02 +0800

    报修

---
+++ Dto.IRepository/IntellRepair/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dto.IRepository.IntellRepair
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        TEntity GetById(Guid id);
+        IQueryable<TEntity> GetAll();
+        void Update(TEntity obj);
+        void Remove(Guid id);
+        int SaveChanges();
+        void Dispose();
+    }
+}
