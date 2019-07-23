a004f53481e86578932f2efbacfda06c71eb78fd
张祎荻 <479663032@qq.com>
2019-07-23 21:45:28 +0800

    Repaire_info更改

--- Dtol/dtol/Repair_Info.cs
+++ Dtol/dtol/Repair_Info.cs
@@ -7,8 +7,11 @@ namespace Dtol.dtol
     public  class Repair_Info
     {
         public int id{get;set;}
+        public int User_InfoId { get; set; }
+        public User_Info User_Info { get; set; }
         public int User_DepartId{get;set;}
         public User_Depart User_Depart{get;set;}
+        public string RepairsTitle{ get; set; }
         public string RepairsType{get;set;}
         public string RepairsEmergency{get;set;}
         public string RepairsContent{get;set;}
