943e4f42ae9111f92d318a12df335521ddc83602
健 柴 <1054254369@qq.com>
2019-08-19 11:08:33 +0800

    分别 登录

--- IntellUser/Controllers/LoginController.cs
+++ IntellUser/Controllers/LoginController.cs
@@ -7,6 +7,7 @@ using Dto.IService.IntellUser;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ViewModel.UserViewModel.RequsetModel;
+using ViewModel.WeChatViewModel.RequestViewModel;
 
 namespace IntellUser.Controllers
 {
@@ -14,35 +15,37 @@ namespace IntellUser.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
-        private readonly ILoginService _ILoginService;
+        //private readonly ILoginService _ILoginService;
+        private readonly ILoginService _LoginService;
         private readonly ILogger _ILogger;
-        public LoginController(ILoginService  loginService, ILogger logger)
+        public LoginController( ILoginService loginService, ILogger logger)
         {
-            _ILoginService = loginService;
+            _LoginService = loginService;
+            //_ILoginService = loginService;
             _ILogger = logger;
         }
-        /// <summary>
-        /// 正式登陆方法
-        /// </summary>
-        /// <param name="loginViewModel">用户名和密码</param>
-        /// <returns></returns>
-        [HttpPost]
-        public ActionResult UserLogin([FromBody] LoginViewModel loginViewModel)
-        {
-          if (!ModelState.IsValid)
-          {
-               
-          }
+        ///// <summary>
+        ///// 正式登陆方法
+        ///// </summary>
+        ///// <param name="loginViewModel">用户名和密码</param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public ActionResult UserLogin([FromBody] LoginViewModel loginViewModel)
+        //{
+        //  if (!ModelState.IsValid)
+        //  {
 
-          else
-          {
-            return Content("输入数据通过验证");
-          }
-            _ILoginService.Login_Valide(loginViewModel);
+        //  }
+
+        //  else
+        //  {
+        //    return Content("输入数据通过验证");
+        //  }
+        //    _ILoginService.Login_Valide(loginViewModel);
 
 
-            return Ok();
-        }
+        //    return Ok();
+        //}
 
 
         /// <summary>
@@ -50,7 +53,7 @@ namespace IntellUser.Controllers
         /// </summary>
         /// <remarks>绝对正确</remarks>
         /// <returns></returns>
-        
+
         [HttpPost]
         public ActionResult UserLoginAbsolute()
         {
@@ -58,11 +61,44 @@ namespace IntellUser.Controllers
             return Ok(
 
                 new {
-                    code="200",
-                    data= tk
+                    code = "200",
+                    data = tk
                 }
                 );
-        }
 
+
+            /// <summary>
+            //     /// 根据用户账号和密码查询用户信息
+            //     /// </summary>
+            //     /// <param name="weChatLoginViewModel"></param>
+            //     /// <returns></returns>
+            //[HttpPost]
+            //public ActionResult Manage_WeChatLogin_User(WeChatLoginViewModel weChatLoginViewModel)
+            //{
+            //    WeChatLoginResModel weChatLoginResModel = new WeChatLoginResModel();
+            //    var UserSearchResult = _LoginService.WeChatLogin_User(weChatLoginViewModel);
+
+            //    if (UserSearchResult == null)
+            //    {
+            //        weChatLoginResModel.IsSuccess = false;
+            //        weChatLoginResModel.baseViewModel.Message = "用户名不存在或者密码错误";
+            //        weChatLoginResModel.baseViewModel.ResponseCode = 400;
+            //        _ILogger.Information("用户名不存在或者密码错误，进入系统失败");
+            //        return BadRequest(weChatLoginResModel);
+            //    }
+            //    else
+            //    {
+            //        weChatLoginResModel.user_session = UserSearchResult;
+            //        weChatLoginResModel.IsSuccess = true;
+            //        weChatLoginResModel.baseViewModel.Message = "存在该用户，查询成功";
+            //        weChatLoginResModel.baseViewModel.ResponseCode = 200;
+            //        _ILogger.Information("查询用户信息，存在该用户，权限查询成功");
+
+            //        return Ok(weChatLoginResModel);
+
+
+            //    }
+            //}
+        }
     }
 }
--- IntellWeChat/Controllers/LoginController.cs
+++ IntellWeChat/Controllers/LoginController.cs
@@ -79,7 +79,50 @@ namespace IntellWeChat.Controllers
                 weChatLoginResModel.baseViewModel.Message = "存在该用户，查询成功";
                 weChatLoginResModel.baseViewModel.ResponseCode = 200;
                 _ILogger.Information("查询用户信息，存在该用户，权限查询成功");
-                return Ok(weChatLoginResModel);
+              
+             return Ok(weChatLoginResModel);
+
+
+            }
+        }
+
+
+
+        /// 根据用户账号和密码查询用户信息(旭旭专用)
+        /// </summary>
+        /// <param name="weChatLoginViewModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Manage_XuXuLogin_User(WeChatLoginViewModel weChatLoginViewModel)
+        {
+            WeChatLoginResModel weChatLoginResModel = new WeChatLoginResModel();
+            var UserSearchResult = _loginService.WeChatLogin_User(weChatLoginViewModel);
+
+            if (UserSearchResult == null)
+            {
+                weChatLoginResModel.IsSuccess = false;
+                weChatLoginResModel.baseViewModel.Message = "用户名不存在或者密码错误";
+                weChatLoginResModel.baseViewModel.ResponseCode = 400;
+                _ILogger.Information("用户名不存在或者密码错误，进入系统失败");
+                return BadRequest(weChatLoginResModel);
+            }
+            else
+            {
+                weChatLoginResModel.user_session = UserSearchResult;
+                weChatLoginResModel.IsSuccess = true;
+                weChatLoginResModel.baseViewModel.Message = "存在该用户，查询成功";
+                weChatLoginResModel.baseViewModel.ResponseCode = 200;
+                _ILogger.Information("查询用户信息，存在该用户，权限查询成功");
+
+                var tk = new { token = "2728b712288da12fffd103af3bd616ff" };
+                return Ok(
+
+                    new
+                    {
+                        code = "200",
+                        data = tk
+                    }
+                    );
 
 
             }
